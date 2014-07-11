8f3c3ec111f5af3252863c5902ec712e34770f5a
Tim Henrich <tim.henrich@gmail.com>
2014-07-11 17:25:37 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'TurretIO',
+    version = '0.1',
+    py_modules = ['TurretIO'],
+    author = 'Turret IO Team',
+    author_email = 'team@turret.io',
+    description = 'Turret.IO Python client library',
+    keywords = 'Turret.IO, turretio, email marketing',
+    url = 'http://turret.io'
+)
